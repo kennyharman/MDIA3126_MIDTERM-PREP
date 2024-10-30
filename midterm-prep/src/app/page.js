45b7080d90f26308590dd12b1a5ceeb35342031a
kennyharman <144394021+kennyharman@users.noreply.github.com>
2024-10-30 13:56:23 -0700

    adding midterm prep lesson

--- midterm-prep/src/app/page.js
+++ midterm-prep/src/app/page.js
@@ -1,101 +1,84 @@
+"use client"
+import { useState } from "react";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+// - Implementation
+// - Pick an API to use
+// - Build a button that has a fetch action
+// - Build a component for displaying our data (should have empty or fulfilled state)
+// - Build a function that does some data fetching
+// - Format and handle data
+// - Style and create breakpoints
+// - Component for our button
+
+  const [pictureContents, setPictureContents] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  async function fetchPictures() {
+    const API_URL = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=5";
+    const response = await fetch (API_URL);
+    const data = await response.json();
+    setPictureContents(data);
+    //setPictureContents("fake data");
+    setLoading(false);
+  }
+
+  const Header = () => {
+    return (
+      <header>
+        <h1>
+          My absolutely huge bungus midterm preparatory website
+        </h1>
+        <button 
+          className="border-2 border-white p-2"
+          onClick={fetchPictures}
+          disabled={loading}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          Fetch
+        </button>
+      </header>
+    );
+  };
+
+  const PictureDisplay = () => {
+    if (loading) {
+      return <section>
+        Loading...
+      </section>
+    }
+
+    if (pictureContents) {
+      const pictureList = [];
+
+      pictureContents.forEach((picture, i) => {
+        pictureList.push(
+          <article key = {i}>
+            <img src={picture.url} alt={picture.explanation}/>
+            <h2>
+              {picture.title}
+            </h2>
+            <p>
+              {picture.explanation}
+            </p>
+          </article>)
+      })
+      return <section>
+        {pictureList}
+      </section>
+    }
+
+    return <section>
+      There have been, infact, no photos fetched
+    </section>
+
+  }
+
+  return (
+    <div className='m-8'>
+      <Header/>
+      <PictureDisplay/>
     </div>
   );
 }
